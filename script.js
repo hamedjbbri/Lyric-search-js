ac79497210cef50d135c0f4aaecf5b46a13edacf
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-01-30 19:01:25 -0800

    get and display song lyrics

--- script.js
+++ script.js
@@ -73,6 +73,19 @@ function showData(data) {
        
         showData(data); }
  
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);   
+        const data = await res.json();
+     
+        const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+        result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <span>${lyrics}</span>`;
+        more.innerHTML = '';
+}
+
 // Event Listeners
 form.addEventListener('submit', e => {
      e.preventDefault();
@@ -84,4 +97,18 @@ form.addEventListener('submit', e => {
      } else {
         searchSongs(searchTerm);
      }
-})
+});
+
+
+result.addEventListener('click', e => {
+
+const clickedEl = e.target;
+
+if (clickedEl.tagName === 'BUTTON') {
+   const artist = clickedEl.getAttribute('data-artist');
+   const songTitle = clickedEl.getAttribute('data-songtitle');
+
+   getLyrics(artist, songTitle);
+}
+
+});
